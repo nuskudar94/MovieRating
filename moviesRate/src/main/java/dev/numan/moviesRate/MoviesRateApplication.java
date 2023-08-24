44749c44ee84173dad278ccd081585e9e5345f8a
nuskudar94 <67208136+nuskudar94@users.noreply.github.com>
2023-08-24 13:07:57 +0200

    first commit

---
+++ moviesRate/src/main/java/dev/numan/moviesRate/MoviesRateApplication.java
@@ -0,0 +1,12 @@
+package dev.numan.moviesRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRateApplication {
+
+	public static void main(String[] args) {SpringApplication.run(MoviesRateApplication.class, args);
+	}
+
+}
---
+++ moviesRate/src/test/java/dev/numan/moviesRate/MoviesRateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.numan.moviesRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
