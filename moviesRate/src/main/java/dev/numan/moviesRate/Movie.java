44b24b2de4b963921a4404d9e1a310eb57410941
nuskudar94 <67208136+nuskudar94@users.noreply.github.com>
2023-08-25 12:41:33 +0200

    Add:Movies

---
+++ moviesRate/src/main/java/dev/numan/moviesRate/Movie.java
@@ -0,0 +1,19 @@
+package dev.numan.moviesRate;
+
+import java.lang.annotation.Documented;
+
+
+@Document(collection="movies")
+public class Movie {
+    private ObjectId id;
+
+    private String imdbId;
+
+    private  id;
+
+    private ObjectId id;
+
+    private ObjectId id;
+
+
+}
--- moviesRate/src/main/java/dev/numan/moviesRate/MoviesRateApplication.java
+++ moviesRate/src/main/java/dev/numan/moviesRate/MoviesRateApplication.java
@@ -2,11 +2,22 @@ package dev.numan.moviesRate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesRateApplication {
 
-	public static void main(String[] args) {SpringApplication.run(MoviesRateApplication.class, args);
+	public static void main(String[] args) {
+
+		SpringApplication.run(MoviesRateApplication.class, args);
+
+	}
+
+	@GetMapping("/root")
+	public String apiRoot(){
+		return "Hello World";
 	}
 
 }
